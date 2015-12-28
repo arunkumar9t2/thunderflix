2c05764eea2ccd9a71b099d4f5d2e50ecdbe15b5
Arunkumar <arunk.beece@gmail.com>
2015-12-28 18:58:38 +0000

    Licenses Added

--- app/src/main/java/arun/com/popularmovies/fragments/MovieGridFragment.java
+++ app/src/main/java/arun/com/popularmovies/fragments/MovieGridFragment.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015.  Arunkumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package arun.com.popularmovies.fragments;
 
 import android.content.Intent;
@@ -22,9 +37,6 @@ import arun.com.popularmovies.models.Movie;
 import arun.com.popularmovies.models.MovieResults;
 import arun.com.popularmovies.tasks.MovieFetcherTask;
 
-/**
- * Created by Arun on 06/12/2015.
- */
 public class MovieGridFragment extends Fragment {
     public static final SparseArray<Drawable> mPosterCache = new SparseArray<>(1);
     private static final String ARG_SORT_TYPE = "sort_type";
--- app/src/main/java/arun/com/popularmovies/tasks/MovieFetcherTask.java
+++ app/src/main/java/arun/com/popularmovies/tasks/MovieFetcherTask.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015.  Arunkumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package arun.com.popularmovies.tasks;
 
 import android.os.AsyncTask;
@@ -9,9 +24,6 @@ import arun.com.popularmovies.services.MovieService;
 import retrofit.Call;
 import retrofit.Response;
 
-/**
- * Created by Arun on 06/12/2015.
- */
 public class MovieFetcherTask extends AsyncTask<Void, Void, MovieResults> {
     public static final String SORT_TYPE_POPULAR = "popularity.desc";
     public static final String SORT_TYPE_RATED = "vote_average.desc";
